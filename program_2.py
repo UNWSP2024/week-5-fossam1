c90f4b272935fa0aae6a9469d3355f595c74e209
fossam1 <fossam1212@gmail.com>
2024-10-04 10:30:30 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,3 +1,7 @@
+# Samuel Foss
+# Program 2: Math Quiz
+# 10/4/2024
+
 # Program #2: Math Quiz
 # Write a program that gives simple math quizzes.  The program should display two random numbers to be added, such as
 
@@ -7,7 +11,18 @@
 
 # ------
 
-# The program should allow the student to enter the answer.  
-# If the answer is correct, a message of congratulations should be displayed.  
-# If the answer is incorrect a message showing the correct answer should be displayed.  
+# The program should allow the student to enter the answer.
+# If the answer is correct, a message of congratulations should be displayed.
+# If the answer is incorrect a message showing the correct answer should be displayed.
 # The program must use a function that accomplishes part of the needed tasks.
+def quiz():
+    answer = int(input("What is 15 + 9? : "))
+    if answer == 24 : print("Congratulations!")
+    else : print("The answer is 24!")
+
+quiz()
+#Output
+#What is 15 + 9? : 24
+#Congratulations!
+
+#Process finished with exit code 0
